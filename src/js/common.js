3347cfb51944a5dc7d765e4f8dc95a91a64124fa
yushinbox@gmail.com <yushinbox@gmail.com>
2021-02-24 05:17:16 +0300

    +

--- src/js/common.js
+++ src/js/common.js
@@ -9,10 +9,147 @@ $(window).on("load", function () {
 });
 
 $(document).ready(function (e) {
-  $(".search-cards-card__title--drop").slideUp(1);
+  // $(".search-cards-card__title--drop").slideUp(1);
   $(".search-cards-card__add").on("click", function () {
     $(this).siblings(".search-cards-card__title--drop").slideDown(200);
     $(this).parent().css("display","block")
     $(this).siblings(".search-cards-card__title").css('display','block');
     $(this).fadeOut(200);
   });});           				  				
+
+
+  (function () {
+     var hint = document.createElement("div");
+    hint.className = "hint"; // изначально невидимый клаcc
+    document.body.appendChild(hint);
+
+    var path = document.getElementsByClassName("path");
+    var i;
+    for (i = 0; i < path.length; i++) {
+      path[i].addEventListener("click", function (e) {
+        // hint.innerText = this.dataset.description;
+
+        hint.innerText = "Скопировано!";
+
+        hint.style.display = "inline-block";
+
+        /* Условие для координат всплывающего блока. 
+    Через (e.pageX + Любое_Число) можно настраивать положение блока */
+        if (e.pageX + hint.offsetWidth < document.body.offsetWidth) {
+          /* e.pageX == расстояние от мышки до левого края страницы, в пикселях
+      hint.offsetWidth == ширина всплывающей подсказки
+      Если их сумма оказывается больше ширины body - сработает else */
+         hint.style.cssText = `
+            opacity: 1; 
+            `;
+
+hint.style.top = e.pageY + 33 + "px";
+hint.style.left = e.pageX  -12 + "px";
+
+ setTimeout(function () {
+
+   hint.style.cssText = `
+            opacity: 0; 
+            `;
+ }, 1000);
+        } else {
+          hint.style.top = e.pageY + 10 + "px";
+          hint.style.left = e.pageX - hint.offsetWidth - 10 + "px";
+        }
+        /* 'e' - всего лишь переменная в фукнции... там могло бы быть
+       function (bubu) { bubu.pageX; } */
+      });
+    }
+
+
+    /* Добавляем событие 'клик' на всю страницу и каждый раз запускаем функцию*/
+    // document.addEventListener("click", function (e) {
+    //   /*Если e.target (кликнутый элемент) имеет класс "path" - разворачиваемся*/
+    //   if (e.target.className.match("path")) {
+    //     return;
+    //   }
+    //   /* А если нет - скрываем блок */
+    //   hint.style.display = "none";
+    // });
+  })();
+
+
+// рандомный цвет карточки из списка
+$(".search-cards-card").mouseenter(function () {
+
+  const color = [
+    "#FDFFBC",
+    "#E7D9EA",
+    "#CFEBD2",
+    "#FCD1D1",
+    "#D3E0EA",
+    "#EFF7E1",
+    "#FFEEBB",
+    "#FFDCDC",
+    "#D3E0DC",
+    "#BEE5D3",
+    "#D3E0DC",
+    "#D6B0B1",
+  ];
+
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+var i = randomInteger(0, 11);
+  var hue = color[i];
+  $(this)
+    .css("background", hue)
+    .find(".search-cards-card__add")
+    .css({ background: hue, "box-shadow": "0px -25px 15px -10px"+ hue  });
+
+});
+
+
+$(".search-cards-card").mouseleave(function () {
+  $(this)
+    .css("background", "white")
+    .find(".search-cards-card__add")
+    .css({ "background": "white", "box-shadow": "0px -25px 15px -10px #fff" });
+});
+
+
+
+
+
+
+
+
+$(document).ready(function (e) {$(".home-section__button").on("click", function () {
+  $(this).siblings(".search__cards--add")
+    .slideDown(300)
+    .css("display", "flex")
+    .find(".search-cards__card--birthday")
+    .css("margin-bottom", "20px");
+  $(this)
+    .attr("disabled", true)
+    // .css({ background: " rgba(158, 185, 244)" })
+    // .find(".button-helper--birthday")
+    // .css("background", " rgba(158, 185, 244)");
+});
+
+});           				  				
+$(document).ready(function (e) {
+  
+  $(".popular__cards--add").slideUp(1);
+  $(".home-section__button").on("click", function () {
+  $(this)
+    .siblings(".popular__cards--add")
+    .slideDown(300)
+    .css("display", "grid")
+    // .find(".search-cards__card--birthday")
+    // .css("margin-bottom", "20px");
+  $(this)
+    .attr("disabled", true)
+    // .css({ background: " rgba(158, 185, 244)" })
+    // .find(".button-helper--birthday")
+    // .css("background", " rgba(158, 185, 244)");
+});
+
+});           				  				
--- src/js/header.js
+++ src/js/header.js
@@ -6,21 +6,36 @@ import $ from "jquery";
 
 $(document).ready(function (e) {
 
+
+
+
+// $(".header__mask").css("transform", "translateX(100%)");
+
+
   $(".header__burger").on("click", function () {
     
-    $(".menu")
-      .addClass("menu-active")
+    $(".menu").addClass("menu-active")
       // .append(
       //   $(
       //     "<div class='header__info info'><a class='info_phone' href='#!'>+7(000) 123 45 65</a><div class='info_search border-bg'><input type='text' placeholder='Поиск' /><i class='icon-search'></i></div></div>"
       //   )
       // );
       
-      setTimeout(function () {
-        $(".info").addClass("info-active");
-      }, 200);
+
+      // setTimeout(function () {
+      //   $(".info").addClass("info-active");
+      // }, 200);
 
     $("body").addClass("lock");
+    $(".menu__item:nth-child(5) ")
+      .find(".menu__link")
+      .text("Открытки и картинки");
+    $(".menu__item:nth-child(6) ")
+      .find(".menu__link")
+      .text("Музыкальные и голосовые");
+$(".header__mask").css("transform", "translateX(calc(100vw - 64px))");
+$(this).css("opacity","0")
+
   }); 
 
 
@@ -32,12 +47,19 @@ $(document).ready(function (e) {
 $(".header__close").on("click", function () {
 $(".menu")
   .removeClass("menu-active")
-  .find(".header__info")
-  .remove();
-  $(".info").removeClass("info-active");
+  // .find(".header__info")
+  // .remove();
+  // $(".info").removeClass("info-active");
 $("body").removeClass("lock");
+ $(".menu__item:nth-child(5) ").find(".menu__link").text("Открытки ");
+ $(".menu__item:nth-child(6) ")
+   .find(".menu__link")
+   .text("Музыкальные ");
+$(".header__mask").css("transform", "translateX(100%)");
+  $(".header__burger").css("opacity", "1");
+  
+  
   });
-
 });
 
  
@@ -59,14 +81,18 @@ $("body").removeClass("lock");
 
 window.onresize = function () {
   if (window.innerWidth >= 768) {
-    $(".menu").removeClass("menu-active").find(".header__info").remove();
-    $(".info").removeClass("info-active");
+    // $(".menu").removeClass("menu-active").find(".header__info").remove();
+    $(".menu").removeClass("menu-active")
+    // $(".info").removeClass("info-active");
     $("body").removeClass("lock");
-// alert("");
-  }
+$(".menu__item:nth-child(5) ").find(".menu__link").text("Открытки ");
+$(".menu__item:nth-child(6) ").find(".menu__link").text("Музыкальные ");
+$(".header__mask").css("transform", "translateX(-100%)");
+$(".header__burger").css("opacity", "1");
+}
 };
 
-
+// переключение языка
 
 $(document).ready(function (e) {
   
